e561e06c7db38d7e083851a84ced68b24e6dcbca
rasmusengelbrecht <32791593+rasmusengelbrecht@users.noreply.github.com>
2024-02-25 15:11:19 +0000

    Added Height Bar chart

--- Hello.py
+++ Hello.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import duckdb
+import pandas as pd
+import altair as alt
 
 st.set_page_config(
     page_title="PokeData",
@@ -16,7 +18,50 @@ SELECT
   *
 FROM my_db.main.pokemon
 """
-df = con.execute(query).df()
+pokemon_df = con.execute(query).df()
 
-st.text(df)
+# Rename the '_name' column to 'pokemon' in the DataFrame
+pokemon_df.rename(columns={'_name': 'Pokemon','height': 'Height'}, inplace=True)
 
+# Sort the DataFrame by height in descending order and select the top 10 tallest Pokémon
+top_10_tallest = pokemon_df.nlargest(10, 'Height')
+
+
+
+# st.data_editor(
+#     top_10_tallest,
+#     column_config={
+#         "sprite_url": st.column_config.ImageColumn(
+#             "Preview Image", help="Streamlit app preview screenshots"
+#         )
+#     },
+#     column_order=["sprite_url", "Height"],
+#     hide_index=True,
+# )
+
+st.divider()
+
+st.write("## The Height of the Top 10 Tallest Pokémon! 📏")
+
+# Create Altair chart for images
+image_chart = alt.Chart(top_10_tallest, height=500).mark_image(
+    width=40,
+    height=40
+).encode(
+    x=alt.X('Pokemon', sort=None),  # Disable sorting to maintain original order
+    y='Height',
+    url='sprite_url'
+)
+
+# Create Altair chart for bars
+bar_chart = alt.Chart(top_10_tallest, height=500).mark_bar(
+    color='dimgrey'
+).encode(
+    x=alt.X('Pokemon', sort=None),  # Disable sorting to maintain original order
+    y='Height'
+)
+
+# Layer the image chart and bar chart
+combined_chart = alt.layer(bar_chart, image_chart)
+
+st.altair_chart(combined_chart, use_container_width=True)
